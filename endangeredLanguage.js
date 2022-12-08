328e845c9c93289ec1e99683ea05bb26a8793829
ConnorLyon <38572427+ConnorLyon@users.noreply.github.com>
2022-12-08 14:03:34 -0800

    Added highlights to hover and click

--- endangeredLanguage.js
+++ endangeredLanguage.js
@@ -197,6 +197,9 @@
                 })
                 .on("click", function (d) {
                     //console.log("Country Clicked:  ", d.properties.name);
+                    d3.select(".selected").classed("selected", false);
+                    //console.log("Selected");
+                    d3.select(this).classed("selected", true);
                     var country_name = d.properties.name;
                     display_Families_Country(country_name);
                 });
